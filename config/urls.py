d9c52e2a8a6c778c2cbfec6f0441d1ce3869424a
alireza <alirezaa073@gmail.com>
2024-02-10 22:51:12 +0330

    dj rest auth third party package added to project. 3 endpoints register/login/logou also added.

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
 
 if settings.DEBUG:
